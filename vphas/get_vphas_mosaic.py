8a9f4fd8a60b0929537e59cf0b780f9bea289125
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-08-12 11:09:21 -0400

    bugfixy

--- vphas/get_vphas_mosaic.py
+++ vphas/get_vphas_mosaic.py
@@ -28,10 +28,6 @@ if len(files) < 732:
     tbl = Eso.query_surveys(surveys='VPHASplus', coord1=0, coord2=0, coord_sys='gal', box=5*u.deg)
     files = Eso.retrieve_data(tbl['ARCFILE'])
 
-hdus = [hdu
-        for h in (fits.open(x)
-                  for x in glob.glob("ADP*.fits*"))
-        for hdu in h if 'CRVAL1' in hdu.header and (260 < hdu.header['CRVAL1'] < 270)]
 #wcs_out, shape_out = find_optimal_celestial_wcs([h[1] for h in hdus], frame='galactic')
 
 wcs_out = wcs.WCS({"CTYPE1": "GLON-CAR",
@@ -84,7 +80,6 @@ output_coverage[0].header.update(wcs_out.to_header())
 final_array = output_file[0].data
 final_footprint = output_coverage[0].data
 
-input_data = hdus
 output_projection = wcs_out
 
 # input parameters we're not using
@@ -105,93 +100,84 @@ wcs_out, shape_out = parse_output_projection(output_projection,
 
 arrays = []
 
-for idata in ProgressBar(range(len(input_data))):
+#hdus = [hdu
+#        for h in (fits.open(x)
+#                  for x in glob.glob("ADP*.fits*"))
+#        for hdu in h if 'CRVAL1' in hdu.header and (260 < hdu.header['CRVAL1'] < 270)]
+for fn in ProgressBar(glob.glob("ADP*fits*")):
+    hdul = fits.open(fn)
+    for hdu in hdul:
+        if not hasattr(hdu, 'data') or ('CRVAL1' not in hdu.header) or (not (260 < hdu.header['CRVAL1'] < 270)):
+            continue
 
-    # We need to pre-parse the data here since we need to figure out how to
-    # optimize/minimize the size of each output tile (see below).
-    array_in, wcs_in = parse_input_data(input_data[idata], hdu_in=hdu_in)
+        # We need to pre-parse the data here since we need to figure out how to
+        # optimize/minimize the size of each output tile (see below).
+        array_in, wcs_in = parse_input_data(hdu)
 
-    # We also get the weights map, if specified
-    if input_weights is None:
-        weights_in = None
-    else:
-        weights_in = parse_input_weights(input_weights[idata], hdu_weights=hdu_weights)
-        if np.any(np.isnan(weights_in)):
-            weights_in = np.nan_to_num(weights_in)
+        # Since we might be reprojecting small images into a large mosaic we
+        # want to make sure that for each image we reproject to an array with
+        # minimal footprint. We therefore find the pixel coordinates of corners
+        # in the initial image and transform this to pixel coordinates in the
+        # final image to figure out the final WCS and shape to reproject to for
+        # each tile. Note that in future if we are worried about significant
+        # distortions of the edges in the reprojection process we could simply
+        # add arbitrary numbers of midpoints to this list.
+        ny, nx = array_in.shape
+        xc = np.array([-0.5, nx - 0.5, nx - 0.5, -0.5])
+        yc = np.array([-0.5, -0.5, ny - 0.5, ny - 0.5])
+        xc_out, yc_out = wcs_out.world_to_pixel(wcs_in.pixel_to_world(xc, yc))
 
-    # Since we might be reprojecting small images into a large mosaic we
-    # want to make sure that for each image we reproject to an array with
-    # minimal footprint. We therefore find the pixel coordinates of corners
-    # in the initial image and transform this to pixel coordinates in the
-    # final image to figure out the final WCS and shape to reproject to for
-    # each tile. Note that in future if we are worried about significant
-    # distortions of the edges in the reprojection process we could simply
-    # add arbitrary numbers of midpoints to this list.
-    ny, nx = array_in.shape
-    xc = np.array([-0.5, nx - 0.5, nx - 0.5, -0.5])
-    yc = np.array([-0.5, -0.5, ny - 0.5, ny - 0.5])
-    xc_out, yc_out = wcs_out.world_to_pixel(wcs_in.pixel_to_world(xc, yc))
+        # Determine the cutout parameters
 
-    # Determine the cutout parameters
+        # In some cases, images might not have valid coordinates in the corners,
+        # such as all-sky images or full solar disk views. In this case we skip
+        # this step and just use the full output WCS for reprojection.
 
-    # In some cases, images might not have valid coordinates in the corners,
-    # such as all-sky images or full solar disk views. In this case we skip
-    # this step and just use the full output WCS for reprojection.
+        if np.any(np.isnan(xc_out)) or np.any(np.isnan(yc_out)):
+            imin = 0
+            imax = shape_out[1]
+            jmin = 0
+            jmax = shape_out[0]
+        else:
+            imin = max(0, int(np.floor(xc_out.min() + 0.5)))
+            imax = min(shape_out[1], int(np.ceil(xc_out.max() + 0.5)))
+            jmin = max(0, int(np.floor(yc_out.min() + 0.5)))
+            jmax = min(shape_out[0], int(np.ceil(yc_out.max() + 0.5)))
 
-    if np.any(np.isnan(xc_out)) or np.any(np.isnan(yc_out)):
-        imin = 0
-        imax = shape_out[1]
-        jmin = 0
-        jmax = shape_out[0]
-    else:
-        imin = max(0, int(np.floor(xc_out.min() + 0.5)))
-        imax = min(shape_out[1], int(np.ceil(xc_out.max() + 0.5)))
-        jmin = max(0, int(np.floor(yc_out.min() + 0.5)))
-        jmax = min(shape_out[0], int(np.ceil(yc_out.max() + 0.5)))
+        if imax < imin or jmax < jmin:
+            continue
 
-    if imax < imin or jmax < jmin:
-        continue
+        wcs_out_indiv = wcs_out[jmin:jmax, imin:imax]
+        shape_out_indiv = (jmax - jmin, imax - imin)
 
-    wcs_out_indiv = wcs_out[jmin:jmax, imin:imax]
-    shape_out_indiv = (jmax - jmin, imax - imin)
+        # TODO: optimize handling of weights by making reprojection functions
+        # able to handle weights, and make the footprint become the combined
+        # footprint + weight map
 
-    # TODO: optimize handling of weights by making reprojection functions
-    # able to handle weights, and make the footprint become the combined
-    # footprint + weight map
+        array, footprint = reproject_function((array_in, wcs_in),
+                                              output_projection=wcs_out_indiv,
+                                              shape_out=shape_out_indiv,
+                                              hdu_in=hdu_in,
+                                              **kwargs)
 
-    array, footprint = reproject_function((array_in, wcs_in),
-                                          output_projection=wcs_out_indiv,
-                                          shape_out=shape_out_indiv,
-                                          hdu_in=hdu_in,
-                                          **kwargs)
 
-    if weights_in is not None:
-        weights, _ = reproject_function((weights_in, wcs_in),
-                                        output_projection=wcs_out_indiv,
-                                        shape_out=shape_out_indiv,
-                                        hdu_in=hdu_in,
-                                        **kwargs)
+        # For the purposes of mosaicking, we mask out NaN values from the array
+        # and set the footprint to 0 at these locations.
+        reset = np.isnan(array)
+        array[reset] = 0.
+        footprint[reset] = 0.
 
-    # For the purposes of mosaicking, we mask out NaN values from the array
-    # and set the footprint to 0 at these locations.
-    reset = np.isnan(array)
-    array[reset] = 0.
-    footprint[reset] = 0.
+        array = ReprojectedArraySubset(array, footprint,
+                                       imin, imax, jmin, jmax)
 
-    # Combine weights and footprint
-    if weights_in is not None:
-        weights[reset] = 0.
-        footprint *= weights
+        array.array[array.footprint == 0] = 0
+        final_array[array.view_in_original_array] += array.array * array.footprint
+        final_footprint[array.view_in_original_array] += array.footprint
+        del array, array_in, hdu, footprint
+        output_file.flush()
+        output_coverage.flush()
 
-    array = ReprojectedArraySubset(array, footprint,
-                                   imin, imax, jmin, jmax)
-
-    array.array[array.footprint == 0] = 0
-    final_array[array.view_in_original_array] += array.array * array.footprint
-    final_footprint[array.view_in_original_array] += array.footprint
-    del array
-    output_file.flush()
-    output_coverage.flush()
+    del hdul
 
 final_array /= final_footprint
 output_file.close()
