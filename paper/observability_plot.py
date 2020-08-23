d539a2c89316b6c8ecd4ba3903707121c0d7db85
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-08-23 15:42:30 -0400

    add obs plot?!

--- paper/observability_plot.py
+++ paper/observability_plot.py
@@ -77,85 +77,9 @@ for thistime,sunpos in zip(times, sun):
     pl.axis(axlims)
     pl.draw()
     pl.show()
-    pl.pause(0.1)
+    pl.pause(0.01)
     fig.savefig(f'observable_zone_{month}.png', bbox_inches='tight')
 
     sc.set_visible(False)
     for cc in con.collections:
         cc.remove()
-
-# A day in July
-thistime = time.Time(2025+7/12+15/31, format='decimalyear')
-target = coordinates.SkyCoord(0*u.deg, -1*u.deg, frame='galactic')
-#sep = coordgrid.separation(sunpos)
-sunpos = coordinates.get_body(body='Sun', time=thistime).galactic
-ell, bee = sunpos.l, sunpos.b
-sc = ax.scatter(ell, bee, marker='*', c='yellow', transform=transform, edgecolor='k', s=200)
-#ax.pcolormesh(ellgrid, beegrid, (sep>85*u.deg) & (sep < 135*u.deg), transform=transform)
-con = ax.contourf(ellgrid, beegrid, (sep>85*u.deg) & (sep < 135*u.deg), levels=[0.5, 1.5], transform=transform, alpha=0.25)
-#ax.contour(ellgrid, beegrid, (sep>85*u.deg) & (sep < 135*u.deg), levels=[0.5, 1.5], transform=transform)
-
-def rectangle(center, width, height, angle):
-    theta = angle
-    tl = center.l - width/2, center.b + height/2
-    tr = center.l + width/2, center.b + height/2
-    br = center.l + width/2, center.b - height/2
-    bl = center.l - width/2, center.b - height/2
-    crds = u.Quantity(list(map(u.Quantity, (tl, bl, br, tr, tl))))
-    rotmat = [[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]
-    return np.inner(crds, rotmat)
-
-for bee in np.arange(-1, 1, 0.1)*u.deg:
-    center = coordinates.SkyCoord(0*u.deg, bee, frame='galactic')
-    rect = rectangle(center=center, width=27.5*u.arcmin, height=27.5*u.arcmin,
-                     angle=center.position_angle(sunpos))
-    ax.plot(*rect.T, transform=transform)
-
-#import regions
-#rect = regions.RectangleSkyRegion(center=target, width=27.5*u.arcmin,
-#                                  height=27.5*u.arcmin,
-#                                  angle=target.position_angle(sunpos))
-#prect = rect.to_pixel(wcs)
-#art = prect.as_artist(transform=ax.get_transform('pixel'))
-#ax.add_artist(art)
-
-month = months[thistime.ymdhms.month-1]
-ax.set_title(month)
-pl.axis(axlims)
-pl.draw()
-pl.show()
-pl.pause(0.1)
-fig.savefig(f'observation_example_july.png', bbox_inches='tight')
-
-
-
-
-
-# https://stackoverflow.com/a/22794126/814354
-# radii = 85*u.deg+[0,50]*u.deg
-# cen = sun[1]
-# theta = np.linspace(0, 2*np.pi, 1000, endpoint=True)
-# ells = np.outer(radii, np.cos(theta)) + cen.l
-# bees = np.outer(radii, np.sin(theta)) + cen.b
-# # in order to have a closed area, the circles
-# # should be traversed in opposite directions
-# ells[1,:] = ells[1,::-1].wrap_at(360*u.deg)
-# bees[1,:] = bees[1,::-1]
-
-
-#circ_l, circ_b = np.sin(theta) * radius + cen.l, np.cos(theta) * radius + cen.b
-#radius = (85+50)*u.deg
-#circ_l2, circ_b2 = np.sin(theta) * radius + cen.l, np.cos(theta) * radius + cen.b
-
-#pl.plot(circ_l, circ_b, transform=transform)
-#pl.plot(circ_l2, circ_b2, transform=transform)
-#pl.fill(circ_l2, circ_b2, transform=transform)
-
-#diffs = np.diff(ells)
-
-# pl.fill(ells[:,:500].ravel(), bees[:,:500].ravel(), transform=transform)
-# pl.fill(ells[:,500:].ravel(), bees[:,500:].ravel(), transform=transform)
-# pl.plot(ells[0], bees[0], transform=transform)
-# pl.plot(ells[1], bees[1], transform=transform)
-
-
