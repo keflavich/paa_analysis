af2c79f27165d4ea2f48b95a4cc2d63dcc18f197
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-09-08 09:16:57 -0400

    observability plot

--- paper/observability_plot.py
+++ paper/observability_plot.py
@@ -3,6 +3,7 @@ from astropy.wcs import WCS
 from astropy.io import fits
 from astropy.utils.data import get_pkg_data_filename
 from astropy.visualization.wcsaxes.frame import EllipticalFrame
+from astropy import convolution
 from matplotlib import patheffects
 from reproject import reproject_from_healpix, reproject_to_healpix
 from astropy import time, coordinates
@@ -10,6 +11,7 @@ from astropy import units as u
 
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as pl
+pl.ion()
 
 # Use the ROSAT image to get an all-sky header
 filename = get_pkg_data_filename('allsky/allsky_rosat.fits')
@@ -24,8 +26,12 @@ fig.clf()
 ax = plt.subplot(projection=wcs, frame_class=EllipticalFrame)
 
 path_effects=[patheffects.withStroke(linewidth=3, foreground='black')]
-ax.coords.grid(color='black')
-ax.coords['glon'].set_ticklabel(color='cyan', path_effects=path_effects)
+#ax.coords.grid(color='black')
+#ax.coords['glon'].set_ticklabel(color='cyan', path_effects=path_effects)
+ax.coords['glon'].set_ticks_visible(False)
+ax.coords['glat'].set_ticks_visible(False)
+ax.coords['glon'].set_ticklabel_visible(False)
+ax.coords['glat'].set_ticklabel_visible(False)
 transform = ax.get_transform('galactic')
 
 im = ax.imshow(array, origin='lower', cmap='gray_r', interpolation='none')
@@ -59,11 +65,27 @@ axlims = pl.axis()
 ellgrid,beegrid = np.mgrid[-180:180,-90:90]
 coordgrid = coordinates.SkyCoord(ellgrid*u.deg, beegrid*u.deg, frame='galactic')
 
-lmc = coordinates.SkyCoord.from_name('LMC').galactic
-ax.scatter(lmc.l, lmc.b, transform=transform, s=500, marker='o', facecolor=(1,0.5,0,0.3), edgecolor='none')
+lmcfh = fits.open('../lmc/LMC_HI_ATCA_peak.fits')
+smlmc = convolution.convolve_fft(lmcfh[0].data.squeeze(), convolution.Gaussian2DKernel(25))
+lmcwcs = WCS(lmcfh[0].header)
+lmc_transform = ax.get_transform(lmcwcs.celestial)
+ax.contourf(smlmc, levels=[13, 30, 1e4], transform=lmc_transform, colors=[(1,165./256,0,0.5), (1,165./256,0,0.75), (1,165./256,0,1)])
+
+#lmc = coordinates.SkyCoord.from_name('LMC').galactic
+#ax.scatter(lmc.l, lmc.b, transform=transform, s=500, marker='o', facecolor=(1,0.5,0,0.3), edgecolor='none')
+
 
 ngc6503 = coordinates.SkyCoord.from_name('ngc6503').galactic
 ax.scatter(ngc6503.l, ngc6503.b, transform=transform, s=500, marker='o', facecolor=(1,0,0.5,0.3), edgecolor='none')
+
+m33 = coordinates.SkyCoord.from_name('M33').galactic
+ax.scatter(m33.l, m33.b, transform=transform, s=100, marker='s', facecolor=(0,1,0.1,0.4), edgecolor='none')
+wlm = coordinates.SkyCoord.from_name('WLM Galaxy').galactic
+ax.scatter(wlm.l, wlm.b, transform=transform, s=100, marker='s', facecolor=(0,0.1,1,0.4), edgecolor='none')
+ngc6822 = coordinates.SkyCoord.from_name('NGC 6822').galactic
+ax.scatter(ngc6822.l, ngc6822.b, transform=transform, s=100, marker='s', facecolor=(1,0.1,0,0.4), edgecolor='none')
+
+ax.plot([50,50,2.5,2.5,-2.5,-2.5,-50,-50, -2.5, -2.5, 2.5, 2.5, 50], [1, -1, -1, -5, -5, -1, -1, 1, 1, 5, 5, 1, 1], transform=transform, color='r')
 
 months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
@@ -77,7 +99,7 @@ for thistime,sunpos in zip(times, sun):
 
     month = months[thistime.ymdhms.month-1]
     print(month, thistime, thistime.ymdhms)
-    ax.set_title(month)
+    #ax.set_title(month)
     pl.axis(axlims)
     pl.draw()
     pl.show()
